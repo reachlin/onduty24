b0fde5d212c031fcf8ca43e02042016f4559d6cb
lincai <lincai@cn.ibm.com>
2016-02-04 08:26:31 +0000

    jade markdown

--- docker/nodejs/server.js
+++ docker/nodejs/server.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const express = require('express');
-
+const marked = require('marked');
+const fs = require('fs');
 // Constants
 const PORT = 8080;
 
@@ -31,9 +32,26 @@ app.get('/ping', function(req, res) {
 });
 */
 
+app.get('/containers_bss_health_check', function(req, res){
+  res.render('containers_bss_health_check', {name: 'containers_bss_health_check'});
+});
 
 app.get('/action/containers_bss_health_check', function(req, res){
-  res.render('containers_bss_health_check');
+  var path = __dirname + "/documentation-pages/docs/runbooks/containers_bss_health_check.md";
+  var md = fs.readFileSync(path, 'utf8');
+  var render = new marked.Renderer();
+  var line = 0;
+  render.html = function(text){
+    var rtn = "<script>alert('enabled');</script>"+text;
+    return rtn;
+  };
+  render.paragraph = function(text){
+    var rtn = text + "[XXX]"+line;
+    line++;
+    return rtn;
+  };
+  var html = marked(md, {renderer: render});
+  res.send(html);
 });
 
 app.listen(PORT);
